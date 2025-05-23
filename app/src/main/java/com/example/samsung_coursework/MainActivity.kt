319949c3fe784c8f0ba71d2c2058ec6482b13749
Kirill <kirill.ryabikin.05@gmail.com>
2025-05-23 18:57:25 +0300

    Добавление фрагментов

--- app/src/main/java/com/example/samsung_coursework/MainActivity.kt
+++ app/src/main/java/com/example/samsung_coursework/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.samsung_coursework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.example.samsung_coursework.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+        val fragmentContainer = findViewById<FrameLayout>(R.id.fragment_container)
+
+        // Обработка нажатий
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FragmentHome())
+                        .commit()
+                    true
+                }
+                R.id.nav_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FragmentSearch())
+                        .commit()
+                    true
+                }
+                R.id.nav_map -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FragmentMap())
+                        .commit()
+                    true
+                }
+                R.id.nav_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FragmentFavorite())
+                        .commit()
+                    true
+                }
+                R.id.nav_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FragmentProfile())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Установите Home как стартовый фрагмент
+        bottomNav.selectedItemId = R.id.nav_home
     }
 }
