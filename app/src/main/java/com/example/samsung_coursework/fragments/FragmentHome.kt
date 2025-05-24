f5c6d8d96adb399f7a67a4ac681fb94c6bce0260
Kirill <kirill.ryabikin.05@gmail.com>
2025-05-24 14:29:58 +0300

    Проверка 2

--- app/src/main/java/com/example/samsung_coursework/fragments/FragmentHome.kt
+++ app/src/main/java/com/example/samsung_coursework/fragments/FragmentHome.kt
@@ -12,7 +12,6 @@ class FragmentHome : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
 
